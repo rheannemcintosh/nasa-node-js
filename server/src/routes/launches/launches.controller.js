b0c67110110b5055d367c28569aa5d0528a6d59a
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2022-04-01 22:42:54 +0100

    Check that the supplied date is valid

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -21,6 +21,13 @@ function httpAddNewLaunch(req, res) {
     }
 
     launch.launchDate = new Date(launch.launchDate);
+
+    if (isNan(launch.launchDate)) {
+        return res.status(400).json({
+            error: 'Invalid launch date'
+        })
+    }
+    
     addNewLaunch(launch);
     return res.status(201).json(launch);
 }
