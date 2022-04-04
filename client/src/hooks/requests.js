368ec4411cbadb6932169f419a3658c43c647c87
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2022-04-04 22:27:28 +0100

    Delete a launch from the front end

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -32,9 +32,11 @@ async function httpSubmitLaunch(launch) {
   }
 }
 
+// Delete launch with given ID.
 async function httpAbortLaunch(id) {
-  // TODO: Once API is ready.
-  // Delete launch with given ID.
+  return await fetch(`${API_URL}/launches/${id}`, {
+    method: "delete",
+  });
 }
 
 export {
