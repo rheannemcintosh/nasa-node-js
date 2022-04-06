13f6621981b7f0d5ecb8d80b1da1b54a74cfa3f6
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2022-04-06 23:34:10 +0100

    Correct isNaN syntax

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -24,7 +24,7 @@ function httpAddNewLaunch(req, res) {
 
     launch.launchDate = new Date(launch.launchDate);
 
-    if (isNan(launch.launchDate)) {
+    if (isNaN(launch.launchDate)) {
         return res.status(400).json({
             error: 'Invalid launch date'
         })
