05b0f305246582db2ed129457a025ecd4ec9306f
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2022-04-08 15:33:01 +0100

    Add test for error classes

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -25,6 +25,13 @@ describe('Test POST /launch', () => {
         target: 'Kepler-186 f',
     };
 
+    const launchDataWithInvalidDate = {
+        mission: 'USS Enterprise',
+        rocket: 'NCC 1701-D',
+        target: 'Kepler-186 f',
+        launchDate: 'm4r',
+    };
+
     test('It should respond with 201 created', async () => {
         const response = await request(app)
             .post('/launches')
@@ -35,15 +42,31 @@ describe('Test POST /launch', () => {
             const requestDate = new Date(completeLaunchData.launchDate).valueOf();
             const responseDate = new Date(response.body.launchDate).valueOf();
             expect(responseDate).toBe(requestDate);
-            
+
             expect(response.body).toMatchObject(launchDataWithoutDate)
     });
 
-    test('It shoudl catch missing required properties', () => {
+    test('It shoudl catch missing required properties', async () => {
+        const response = await request(app)
+            .post('/launches')
+            .send(launchDataWithoutDate)
+            .expect('Content-Type', /json/)
+            .expect(400);
 
+        expect(response.body).toStrictEqual({
+            error: 'Missing required launch data',
+        });
     });
 
-    test('It should catch invalid dates', () => {
+    test('It should catch invalid dates', async () => {
+        const response = await request(app)
+        .post('/launches')
+        .send(launchDataWithInvalidDate)
+        .expect('Content-Type', /json/)
+        .expect(400);
 
+        expect(response.body).toStrictEqual({
+            error: 'Invalid launch date',
+        });
     });
 });
